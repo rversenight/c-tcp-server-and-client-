9d971962f1c265f00efabf447cac24635961b45e
AlperDibi <ahmetdibi@gmail.com>
2026-05-15 08:25:43 +0300

    Add files via upload

---
+++ CLİENT(uptaded).cpp
@@ -0,0 +1,63 @@
+﻿#include <iostream>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <string>
+
+#pragma comment(lib, "ws2_32.lib")
+
+int main() {
+    WSADATA wsa;
+    if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) {
+        std::cout << "Winsock baslatilamadi!" << std::endl;
+        return 1;
+    }
+
+    SOCKET clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (clientSocket == INVALID_SOCKET) {
+        std::cout << "Socket olusturulamadi!" << std::endl;
+        return 1;
+    }
+
+    sockaddr_in serverAddress;
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(8080);
+
+    // IP adresini metinden ikili formata çeviriyoruz
+    if (inet_pton(AF_INET, "127.0.0.1", &serverAddress.sin_addr) <= 0) {
+        std::cout << "Gecersiz adres!" << std::endl;
+        return 1;
+    }
+
+    // Server'a bağlanmayı dene
+    std::cout << "Server'a baglaniliyor..." << std::endl;
+    if (connect(clientSocket, (sockaddr*)&serverAddress, sizeof(serverAddress)) < 0) {
+        std::cout << "Baglanti hatasi! Server acik mi?" << std::endl;
+        return 1;
+    }
+    std::cout << "Baglanti basarili!" << std::endl;
+
+    char mesaj[1024];
+    char cevap[1024];
+
+    while (true) {
+        std::cout << "Alper: ";
+        std::cin.getline(mesaj, 1024);
+
+        if (strlen(mesaj) == 0) continue; // Boş mesaj gönderme
+
+        send(clientSocket, mesaj, (int)strlen(mesaj), 0);
+
+        int bytes = recv(clientSocket, cevap, 1024, 0);
+        if (bytes <= 0) {
+            std::cout << "Baglanti koptu veya Server kapandi.\n";
+            break;
+        }
+
+        cevap[bytes] = '\0';
+        std::cout << "Server: " << cevap << std::endl;
+    }
+
+    closesocket(clientSocket);
+    WSACleanup();
+    return 0;
+}
---
+++ Server(uptaded).cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+#include <winsock2.h>
+
+#pragma comment(lib, "ws2_32.lib")
+
+int main() {
+    WSADATA wsa;
+    WSAStartup(MAKEWORD(2, 2), &wsa);
+
+    SOCKET serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+
+    sockaddr_in serverAddress;
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
+    serverAddress.sin_port = htons(8080);
+
+    bind(serverSocket, (sockaddr*)&serverAddress, sizeof(serverAddress));
+    listen(serverSocket, 3);
+
+    std::cout << "Client gelmesini bekliyoruz (Port: 8080)...\n";
+
+    SOCKET clientSocket = accept(serverSocket, NULL, NULL);
+    if (clientSocket == INVALID_SOCKET) {
+        std::cout << "Baglanti kabul edilemedi!\n";
+        return 1;
+    }
+
+    std::cout << "Client kral bagli su an!\n";
+
+    char buffer[1024];
+
+    while (true) {
+        memset(buffer, 0, sizeof(buffer));
+
+        // recv fonksiyonu kaç byte geldiğini döndürür
+        int bytesReceived = recv(clientSocket, buffer, 1024, 0);
+
+        if (bytesReceived <= 0) {
+            std::cout << "\nClient baglantiyi kesti veya hata olustu.\n";
+            break;
+        }
+
+        std::cout << "Client: " << buffer << std::endl;
+
+        // Server cevap yazıyor
+        std::cout << "Sen: ";
+        std::cin.getline(buffer, 1024);
+
+        send(clientSocket, buffer, (int)strlen(buffer), 0);
+    }
+
+    closesocket(clientSocket);
+    closesocket(serverSocket);
+    WSACleanup();
+
+    std::cout << "Server kapatildi. Cikmak icin bir tusa basin...";
+    std::cin.get();
+    return 0;
+}
